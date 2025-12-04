b4d3b95f7a435c3cd847e06c6328d0884db20dff
Jean-Emmanuel Perramant <jperrama@gmail.com>
2025-12-04 18:03:18 +0100

    Initial Commit

---
+++ Focus/FocusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusApp.swift
+//  Focus
+//
+//  Created by Jean-Emmanuel on 04/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct FocusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
